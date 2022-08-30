28f9d89d5dd9dd33a33089182ebb9ffd0f275d86
Bangbiu <Empty_Hundred@outlook.com>
2022-08-30 13:47:47 -0500

    Copy Start Graphics patch

--- tester/test_Stage.js
+++ tester/test_Stage.js
@@ -23,6 +23,7 @@ export function run_Stage() {
     stage = new Stage({ canvas: canv });
 
     let btn1 = new CanvasButton({
+        draggable: true,
         frame: [0.5,0.5,0.5,0.5],
         fontSize: 5,
         caption: "Nope",
@@ -41,5 +42,17 @@ export function run_Stage() {
 function render(timestamp) {
     ctx.clearRect(0,0,2000,2000);
     stage.render(ctx);
+
+    ctx.fillStyle = "blue";
+
+    let path = new Path2D();
+
+    path.moveTo(0,0);
+    path.lineTo(100,100);
+    path.lineTo(500,100);
+    path.closePath();
+
+    ctx.fill(path);
+
     window.requestAnimationFrame(render);
 }
